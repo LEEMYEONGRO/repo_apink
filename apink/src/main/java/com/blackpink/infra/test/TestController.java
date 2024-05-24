58bafafe917f4667f1224fc970fd800880d889c0
LEEMYEONGRO <dlaudfh01@gmail.com>
2024-05-24 14:40:33 +0900

    api test중

--- apink/src/main/java/com/blackpink/infra/test/TestController.java
+++ apink/src/main/java/com/blackpink/infra/test/TestController.java
@@ -4,13 +4,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
@@ -20,8 +19,8 @@ public class TestController {
 	@RequestMapping(value = "/testList")
 	public String testList(Model model) throws Exception {
 		
-		String apiUrl = "http://apis.data.go.kr/1471000/CovidDagnsRgntProdExprtStusService/getMmCovidDagnsRgntExprtStusInq?serviceKey=oTO8CGvbxM3M1rTITP6utisQ6gOjYvwqWznKivddm46vqtg8lpLpqIkA9GW0lc3zbHifPhWKruCWvnaQzDlWdA%3D%3D&numOfRows=3&pageNo=1&type=json";
-
+		String apiUrl = "https://ecos.bok.or.kr/api/KeyStatisticList/sample/json/kr/2/5";
+		
 		URL url = new URL(apiUrl);
 		HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 		httpURLConnection.setRequestMethod("GET");
@@ -39,50 +38,20 @@ public class TestController {
 			System.out.println("line: " + line);
 			stringBuilder.append(line);
 		}
-		
+
 		bufferedReader.close();
 		httpURLConnection.disconnect();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode node = objectMapper.readTree(stringBuilder.toString());
 		
-      ObjectMapper objectMapper = new ObjectMapper();
-      
-      Map<String, Object> map = objectMapper.readValue(stringBuilder.toString(), Map.class);
-      
-      System.out.println("######## Map");
-		for (String key : map.keySet()) {
-			String value = String.valueOf(map.get(key));	// ok
-			System.out.println("[key]:" + key + ", [value]:" + value);
-		}
+//		System.out.println("node.get(\"header\").get(\"resultCode\").asText(): " + node.get("header").get("resultCode").asText());
+//		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("header").get("resultMsg").asText());
+//		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(0).get("KIT_PROD_QTY").asText());
 		
-		Map<String, Object> header = new HashMap<String, Object>();
-		header = (Map<String, Object>) map.get("header");
+		model.addAttribute("node", node);
 		
-		System.out.println("######## Header");
-		for (String key : header.keySet()) {
-			String value = String.valueOf(header.get(key));	// ok
-			System.out.println("[key]:" + key + ", [value]:" + value);
-		}
-		
-		String aaa = (String) header.get("resultCode");
-		
-		System.out.println("header.get(\"resultCode\"): " + header.get("resultCode"));
-		System.out.println("header.get(\"resultMsg\"): " + header.get("resultMsg"));
-		
-		Map<String, Object> body = new HashMap<String, Object>();
-		body = (Map<String, Object>) map.get("body");
-		
-//		List<Home> items = new ArrayList<Home>();
-//		items = (List<Home>) body.get("items");
-//		
-//		System.out.println("items.size(): " + items.size());
-//		
-//		System.out.println("items.toString(): " + items.toString());
-		
-//		model.addAllAttributes(map);
-		model.addAllAttributes(header);
-		model.addAllAttributes(body);
-		
-//		model.addAttribute("header", header);
-//		model.addAttribute("body", body);
+//		model.addAttribute(node);
 		
 		return "v1/infra/test/testList";
 	}
