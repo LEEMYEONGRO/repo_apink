56845138254934ba158f0406a7ede8adbba5520f
LEEMYEONGRO <dlaudfh01@gmail.com>
2024-05-29 16:37:31 +0900

    목록 Excel로 다운로드

--- apink/src/main/java/com/blackpink/infra/codegroup/CodeGroupController.java
+++ apink/src/main/java/com/blackpink/infra/codegroup/CodeGroupController.java
@@ -1,5 +1,14 @@
 package com.blackpink.infra.codegroup;
 
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -10,6 +19,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.blackpink.common.constants.Constants;
 import com.blackpink.common.util.UtilDateTime;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class CodeGroupController {
@@ -51,6 +62,84 @@ public class CodeGroupController {
 		return "v1/infra/codeGroupXdm/codeGroupXdmAjaxLita";
 	}
 	
+	@RequestMapping("excelDownload")
+    public void excelDownload(CodeGroupVo vo, HttpServletResponse httpServletResponse) throws Exception {
+		
+		setSearch(vo); 
+		
+		vo.setParamsPaging(service.selectOneCount(vo));
+
+		if (vo.getTotalRows() > 0) {
+			List<CodeGroupDto> list = service.selectList(vo);
+			
+			Workbook workbook = new HSSFWorkbook();	// for xls
+//	        Workbook workbook = new XSSFWorkbook();
+	        Sheet sheet = workbook.createSheet("첫번째 시트");
+	        CellStyle cellStyle = workbook.createCellStyle();        
+	        Row row = null;
+	        Cell cell = null;
+	        int rowNum = 0;
+			
+//	        each column width setting
+	        sheet.setColumnWidth(0, 4100);
+	        sheet.setColumnWidth(1, 4100);
+	        sheet.setColumnWidth(2, 5100);
+	        sheet.setColumnWidth(3, 5100);
+	        sheet.setColumnWidth(4, 2100);
+
+//	        Header
+	        String[] tableHeader = {"코드그룹 코드", "코드그룹 이름", "등록일", "수정일", "삭제여부"};
+
+	        row = sheet.createRow(rowNum++);
+			for(int i=0; i<tableHeader.length; i++) {
+				cell = row.createCell(i);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+				cell.setCellValue(tableHeader[i]);
+			}
+
+//	        Body
+	        for (int i=0; i<list.size(); i++) {
+	            row = sheet.createRow(rowNum++);
+	            
+//	            String type: null 전달 되어도 ok
+//	            int, date type: null 시 오류 발생 하므로 null check
+//	            String type 이지만 정수형 데이터가 전체인 seq 의 경우 캐스팅
+	            
+	            cell = row.createCell(0);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+	            cell.setCellValue(Integer.parseInt(list.get(i).getCgSeq()));
+	            
+	            cell = row.createCell(1);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+	        	cell.setCellValue(list.get(i).getCgName());
+	        	
+	        	cell = row.createCell(2);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+	        	if(list.get(i).getCgModificationDate() != null) cell.setCellValue(UtilDateTime.dateTimeToString(list.get(i).getCgModificationDate()));
+	        	
+	        	cell = row.createCell(3);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+	        	if(list.get(i).getCgCreationDate() != null) cell.setCellValue(UtilDateTime.dateTimeToString(list.get(i).getCgCreationDate()));
+     
+	            cell = row.createCell(4);
+	            cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	            cell.setCellStyle(cellStyle);
+	            if(list.get(i).getCgDeleteNy() != null) cell.setCellValue(list.get(i).getCgDeleteNy() == 0 ? "N" : "Y");
+	        }
+
+	        httpServletResponse.setContentType("ms-vnd/excel");
+	        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=example.xls");	// for xls
+//	        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=example.xlsx");
+
+	        workbook.write(httpServletResponse.getOutputStream());
+	        workbook.close();
+		}
+    }
 	
 	@RequestMapping(value = "/codeGroupXdmAddition")
 	public String codeGroupXdmAddition(CodeGroupDto dto, Model model ) {
